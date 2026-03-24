b5c6a42c43296d2d368acbdc3aeef50b521883db
Vardan Keshishyan <138354187+VardanKeshishyan@users.noreply.github.com>
2026-03-24 02:35:27 -0700

    Comment out data processing code in cut.py
    Comment out the code for processing recipes and reviews.

--- cut.py
+++ cut.py
@@ -1,9 +1,9 @@
-import pandas as pd
+#import pandas as pd
 
-recipes = pd.read_csv("data/recipes.csv")
-recipes.head(200000).to_csv("data/recipes_small.csv", index=False)
+#recipes = pd.read_csv("data/recipes.csv")
+#recipes.head(90000).to_csv("data/recipes_small.csv", index=False)
 
-reviews = pd.read_csv("data/reviews.csv")
-reviews.head(150000).to_csv("data/reviews_small.csv", index=False)
+#reviews = pd.read_csv("data/reviews.csv")
+#reviews.head(150000).to_csv("data/reviews_small.csv", index=False)
 
-print("Done")
+#print("Done")
