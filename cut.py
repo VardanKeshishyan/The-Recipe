f7064604606521017c4263d4050cce463dbec8cb
Vardan Keshishyan <vardankeshishyan1@gmail.com>
2026-03-24 03:44:43 -0700

    app and cut
    delet

--- cut.py
+++
@@ -1,9 +0,0 @@
-#import pandas as pd
-
-#recipes = pd.read_csv("data/recipes.csv")
-#recipes.head(90000).to_csv("data/recipes_small.csv", index=False)
-
-#reviews = pd.read_csv("data/reviews.csv")
-#reviews.head(150000).to_csv("data/reviews_small.csv", index=False)
-
-#print("Done")
